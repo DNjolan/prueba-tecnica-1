b359e04de611b523cacabeb389f0e4a99487e73b
DNjolan <dnjolan546@gmail.com>
2024-12-03 16:15:46 -0300

    Modificación del código, finalizado.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,14 @@
 // Custome Hook
 import useRandomFact from "../customeHook/useRandomeFact"
-import useRandomCat from "../customeHook/useRandomeCat"
 
 export default function App() {
     const { fact, handleRandomFact } = useRandomFact()
-    const { urlRandomCat } = useRandomCat(fact)
-
-    const handleRefreshCat = () => {
-        handleRandomFact()
-    }
-
+    
     return(
         <main>
-            <button onClick={handleRefreshCat}>Get Random Cat</button>
+            <button onClick={handleRandomFact}>Get Random Cat</button>
             <h1>{fact && <p>{fact}</p>}</h1>
-            {urlRandomCat && <img src={urlRandomCat} alt="Obtener random cat with three first words, by id." />}
+            {fact && <img src={'https://cataas.com/cat/says/'+fact} alt={"Obtener un gato random con tres palabras: "+fact} />}
         </main>
     )
 }
--- src/services/facts.ts
+++ src/services/facts.ts
@@ -1,16 +1,8 @@
 const URL_FACTS_RANDOM: string = 'https://catfact.ninja/fact'
-const URL_GET_IMG_RANDOM: string = 'https://cataas.com/cat/says/'
 
 export async function getRandomFact() {
     const res = await fetch(URL_FACTS_RANDOM)
     const data = await res.json()
     const threeWords = data.fact.split(' ', 3).join(' ')
     return threeWords
-}
-
-export async function getRandomImgById( fact: string ) {
-    const paramsUrl = `${URL_GET_IMG_RANDOM}${fact}?fontSize=50&fontColor=red&json=true`
-    const res = await fetch(paramsUrl)
-    const data = await res.json()
-    return `https://cataas.com/cat/${data._id}/says/${fact}?fontSize=50&fontColor=red`
 }
